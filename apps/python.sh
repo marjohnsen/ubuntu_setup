cbee784aaa26834ae45ed0f56bb3316a81fe5dff
Marius Johnsen <marius.johnsen@outlook.com>
2024-10-06 21:45:09 +0200

    app/python/pyenv install

---
+++ apps/python.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+dependencies=("packages","zsh")
+
+install_build_dependencies(){
+    sudo apt update
+    sudo apt install build-essential libssl-dev zlib1g-dev \
+    	libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev \
+	xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+}
+
+
+install_pyenv(){
+    rm -rf $HOME/.pyenv
+    curl https://pyenv.run | bash
+    pyenv update
+}
+
+install_latest_stable(){
+    latest_stable=$(pyenv install --list | grep -E '^\s*3\.[0-9]+\.[0-9]+$' | tail -1 | xargs)
+    pyenv install "$latest_stable"
+    pyenv global "$latest_stable"
+}
+
+main(){
+    install_build_dependencies
+    install_pyenv
+    install_latest_stable
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
+
